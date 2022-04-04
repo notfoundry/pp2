53198dc6e106ce0df9f3f6107f93b59caaca7f04
Mark Johnson <foundry@protonmail.com>
2022-04-04 16:44:59 -0400

    finish documentation update github action

--- .github/actions/build-and-publish-docs/entrypoint.sh
+++ .github/actions/build-and-publish-docs/entrypoint.sh
@@ -5,10 +5,6 @@ WORKSPACE=/github/workspace
 
 OUTPUT_BRANCH=$1
 
-ConfigureGitUser() {
-    git config user.name github-actions[bot]
-    git config user.email github-actions[bot]@users.noreply.github.com
-}
 
 cd ${WORKSPACE}
 mkdir build && cd build
@@ -19,4 +15,10 @@ cd ${WORKSPACE}
 git remote update
 git checkout "${OUTPUT_BRANCH}"
 mv -f ${WORKSPACE}/build/docs/sphinx/* ${WORKSPACE}
-ls ${WORKSPACE}
+
+
+git add .
+git config user.name github-actions[bot]
+git config user.email github-actions[bot]@users.noreply.github.com
+git commit -m "Auto-update documentation from ${GITHUB_REF}@${GITHUB_SHA}"
+git push
