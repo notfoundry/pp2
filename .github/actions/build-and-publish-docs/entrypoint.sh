78072e1550a5f63196b294809ac71fe579c44944
Mark Johnson <foundry@protonmail.com>
2022-04-04 16:26:44 -0400

    doc publication dockerfile testing

--- .github/actions/build-and-publish-docs/entrypoint.sh
+++ .github/actions/build-and-publish-docs/entrypoint.sh
@@ -3,6 +3,13 @@ set -x
 
 OUTPUT_BRANCH=$1
 
+ConfigureGitUser() {
+    git config user.name github-actions[bot]
+    git config user.email github-actions[bot]@users.noreply.github.com
+}
+
 mkdir build && cd build
 cmake /github/workspace
 make Sphinx
+
+cd /github/workspace && git checkout ${OUTPUT_BRANCH}
