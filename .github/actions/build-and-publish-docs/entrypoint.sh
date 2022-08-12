4378dff22e2a0344557cc2e62a7eed83893c0ac9
Mark Johnson <1994508+notfoundry@users.noreply.github.com>
2022-08-12 14:39:02 -0400

    Update entrypoint.sh

--- .github/actions/build-and-publish-docs/entrypoint.sh
+++ .github/actions/build-and-publish-docs/entrypoint.sh
@@ -18,7 +18,7 @@ git checkout "${OUTPUT_BRANCH}"
 git ls-files | xargs rm -f
 git ls-tree -d --name-only "${OUTPUT_BRANCH}" | xargs rm -rf
 mv ${WORKSPACE}/build/docs/sphinx/* ${WORKSPACE}
-rm -rf ${WORKSPACE}/build
+rm -rf ${WORKSPACE}/build  ${WORKSPACE}/docs
 touch ${WORKSPACE}/.nojekyll
 
 
